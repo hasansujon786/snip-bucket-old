b7e637c5551a3cfb72f8f945a693a677444324d1
Hasan Mahmud <hasansujon786@gmail.com>
2019-05-08 16:02:32 +0600

    add fetch & update db

--- src/components/ContentsChild.vue
+++ src/components/ContentsChild.vue
@@ -33,7 +33,28 @@ export default {
   data() {
     return {}
   },
+  methods: {
+    makeReqToApi() {
+      let lang = this.$route.params.lang
+      fetch(`https://raw.githubusercontent.com/hasansujon786/the-doc/master/data/lang/${lang}.json`)
+        .then(response => {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' + response.status)
+            return
+          }
+          // Examine the text in the response
+          response.json().then(data => {
+            console.log(data)
+            this.allLanguags = data
+          })
+        })
+        .catch(err => {
+          console.log('Fetch Error :-S', err)
+        })
+    }
+  },
   created() {
+    this.makeReqToApi()
     console.log(this.$route.params)
     console.log('contentChild creted')
   },
